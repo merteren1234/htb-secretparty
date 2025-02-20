c773cf9c664ab0f9f5bda706816081a5358cc3df
merteren1234 <118080184+merteren1234@users.noreply.github.com>
2025-02-20 23:20:15 +0300

    Delete send3.js

--- send3.js
+++
@@ -1,9 +0,0 @@
-fetch("https://d7f2-24-133-64-217.ngrok-free.app", {
-      method: "get",
-      headers: new Headers({
-        "ngrok-skip-browser-warning": "69420",
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
